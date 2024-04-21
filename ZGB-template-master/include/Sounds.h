74731c3aaf607938b271a17bdd2cfc64ddba27cd
Andrea Bogazzi <abogazzi@shutterstock.com>
2024-04-22 01:19:46 +0200

    broke

---
+++ ZGB-template-master/include/Sounds.h
@@ -0,0 +1,23 @@
+#ifndef SOUNDS_H
+#define SOUNDS_h
+
+#include <gbdk/platform.h>
+#include <Sound.h>
+#include <stdint.h>
+
+#define fxC_4 0x83
+#define fxD_4 0x90
+#define fxE_4 0x9d
+#define fxF_4 0xa2
+#define fxG_4 0xac
+#define fxA_4 0xb6
+#define fxB_4 0xbe
+#define fxC_5 0xc1
+
+#define EMERALD_DING_GAP_DURATION 13
+#define EMERALD_SCALE_TIMER 45
+#define EMERALD_DING_QTY 7
+
+#define fxNotePos 11
+
+#endif
